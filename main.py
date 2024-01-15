cd2943d7a757e18a0cb7c787a2c24ce55f4fee3f
rogachdev <chatbot.ti.developer@gmail.com>
2024-01-15 12:05:10 -0300

    feat: add for file comments

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from gtts import gTTS
 import os
 
+# abre o arquivo txt na raiz do proejto.
 file = open("text.txt", "r").read()
 
+# converte o arquivo txt na linguagem pt-br
 speech = gTTS(text=file, lang='pt-br', slow=False)
+
+# cria uma versáo .mp3 do arquivo txt e salva na raiz do projeto
 speech.save("textVoice.mp3")
+
+#abre o arquivo criado na estensão .mp3
 os.system("textVoice.mp3")
